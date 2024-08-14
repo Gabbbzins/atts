5298c5da15847cb97e733da9d005a9f879766168
Gabriel Sousa <162443949+Gabbbzins@users.noreply.github.com>
2024-08-14 07:51:15 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,8 @@
+let dia = "20/09/2021"
+let documento ="20/09/2021"
+
+if(documento === dia ){
+    console.log("Está na validade")
+}else{
+    console.log("Venceuuuuuuuuuuuu")
+}
---
+++ ex2.js
@@ -0,0 +1,8 @@
+let saldo = 20
+let conta = 180
+
+if(saldo >= conta){
+    console.log("Você pode pagar!")
+}else{
+    console.log("Pobre imundo.")
+}
